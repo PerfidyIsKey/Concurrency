de247714de4a0ccaa620af8c5b8e6e4f3e9fed62
PerfidyIsKey <myday.games@gmail.com>
2018-09-05 13:43:44 +0200

    initial

---
+++ src/API.java
@@ -0,0 +1,16 @@
+import java.util.ArrayList;
+
+public class API {
+    public static void main(String[] args) {
+        new API().run();
+    }
+    private ArrayList<Integer> AmountOfNumbersArray = new ArrayList<>();
+
+    private void run() {
+
+    }
+
+    private ArrayList<Integer> selectionSort(){
+        return AmountOfNumbersArray;
+    }
+}
